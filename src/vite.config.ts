8be3c0b496ca892e7cfe345b5345d04300bfa04a
Chris Wood <chrispychris3@gmail.com>
2022-10-17 09:02:24 -0700

    Add pluginListBlogPosts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -2,26 +2,30 @@ import { defineConfig } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
 import react from "@vitejs/plugin-react";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import pluginListBlogPosts from "../plugins/rollup-plugin-list-blog-posts";
 
 export default defineConfig({
     build: {
-      emptyOutDir: true,
-      outDir: "../build",
+        emptyOutDir: true,
+        outDir: "../build",
     },
     plugins: [
-      createHtmlPlugin({
-        minify: true,
-        entry: "index.tsx",
-      }),
-      react({
-        include: "**/*.{jsx,tsx}",
-      }),
-      viteStaticCopy({
-          targets: [{
-              src: "blog_posts/*",
-              dest: "blog_posts"
-          }]
-      }),
+        createHtmlPlugin({
+            minify: true,
+            entry: "index.tsx",
+        }),
+        react({
+            include: "**/*.{jsx,tsx}",
+        }),
+        viteStaticCopy({
+            targets: [{
+                src: "blog_posts/*",
+                dest: "blog_posts"
+            }]
+        }),
+        pluginListBlogPosts({
+            directory: "src/blog_posts/",
+        }),
     ],
     preview: {
       port: 5173,
