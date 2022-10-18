d8b0a289377dc062cf93dd41acc64b1b989f2ac3
Chris Wood <chrispychris3@gmail.com>
2022-10-17 20:15:27 -0700

    Add plugin to output blog posts to JSON file

--- plugins/rollup-plugin-list-blog-posts.ts
+++ plugins/rollup-plugin-list-blog-posts.ts
@@ -4,8 +4,12 @@ export default function pluginListBlogPosts(options: { directory: string } = { d
     return {
         name: "plugin-list-blog-posts",
         async buildEnd(){
-            const blogFiles: {} = await fs.readdir(options.directory)
-            console.log(blogFiles);
+            const fileName: string = "blog_files.json";
+            let blogFiles: string[] = await fs.readdir(options.directory)
+            blogFiles = blogFiles.filter(file => file.toString() !== fileName);
+            const blogFileListJson: string = JSON.stringify(blogFiles);
+            fs.writeFile("src/blog_posts/" + fileName, blogFileListJson);
+            console.log("Exporting blogfiles and " + fileName + ".");
         },
     };
 };
