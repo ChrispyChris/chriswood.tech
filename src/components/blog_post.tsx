f82ce383eeb305e4ffc5f5b40058802e668af6df
Chris Wood <chrispychris3@gmail.com>
2022-10-19 18:38:34 -0700

    Implement basic blog post component

--- src/components/blog_post.tsx
+++ src/components/blog_post.tsx
@@ -2,28 +2,24 @@ import React, { ReactElement, useEffect, useState } from "react";
 import { marked } from "marked";
 import "../sass/blog_post.scss";
 
-export default function BlogPostsList(): ReactElement {
+export default function BlogPost({ blogPostTitle }: { blogPostTitle: string }): ReactElement {
     const [blogPost, setBlogPost] = useState<string>("");
+    const [blogTitle, setBlogTitle] = useState<string>(blogPostTitle);
 
-    useEffect(() => {
-        async function loadBlogPostTitles(blogPostTitlesJsonURI: string): Promise<string[]>  {
-        let blogPostTitlesFetch = await fetch(`../blog_posts/${blogPostTitlesJson}`);
-        let blogPostsData = await blogPostTitlesFetch.text();
-        setBlogPost(marked.parse(blogPostsData));
-        return blogPostsData;
-        }
+    const fetchBlogPost = () => {
+            async () => { 
+            const blogPostText = await fetch("../blog_posts/" + blogTitle).then(fetchedPost => fetchedPost.text());
+            setBlogPost(blogPostText);
+            }
+    }
 
-        loadBlog("first_blog_post.md")
-    });
+    const handleClick = () => {
+        fetchBlogPost();
+    }
 
     return (
-        <div className="blog__post" dangerouslySetInnerHTML={{__html: blogPost}}></div>
+        <div onClick={handleClick} className="blog__post" dangerouslySetInnerHTML={{__html: blogPost}}></div>
     );
-}
-
-async function loadBlogPostTitles() {
-    let blogPostTitles: string = await fetch("../blog_posts/blog_files.json");
-    
 }
 
 const walkTokens = (token: any) => {
