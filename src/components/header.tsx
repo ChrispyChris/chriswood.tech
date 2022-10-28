d4947c11cb01bc83f5009e6318927f3bf1da6f72
Chris Wood <chrispychris3@gmail.com>
2022-10-28 13:58:21 -0700

    Remove bad code and clean CSS

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,12 @@
 import React, { ReactElement } from "react";
 import MobileNav from "./mobile_nav";
 import Nav from "./nav";
-import useMobileComponent from "../hooks/useMobileComponent.js";
 
 export default function Header(): ReactElement {
-
-    const isMobile: boolean = useMobileComponent(575);
     return(
-      <>
-      {isMobile ? <MobileNav /> : <Nav />}
-      </>
+    <>
+    <MobileNav />
+    <Nav />
+    </>
     );
 }
--- src/components/mobile_nav.tsx
+++ src/components/mobile_nav.tsx
@@ -11,14 +11,9 @@ export default function MobileNav(): ReactElement {
       setIsOpen(isOpen => !isOpen);
     };
 
-    useEffect(() => {
-      document.body.classList.toggle('.hide', isOpen);
-      document.getElementById("mobile-nav_hamburger")!.classList.toggle("hamburger-transition", isOpen);
-    }, [isOpen]);
-
     return(
       <nav className="mobile-nav-container">
-        <ul id="mobile-nav_hamburger" className={`mobile-nav-hamburger--${isOpen ? "open" : "closed"}`}>
+        <ul id="mobile-nav_hamburger" className={`mobile-nav-hamburger--${isOpen ? "open" : "closed"} ${isOpen ? "hamburger-transition" : null}`}>
           {navItems.map(item => <li className="mobile-nav__item" key={item.name} onClick={toggleMenu}><Link to={item.url} className="mobile-nav__link">{item.name}</Link></li>)}
         </ul>
         <HamburgerMenu open={isOpen} click={toggleMenu}/>
