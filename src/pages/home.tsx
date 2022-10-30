dc6ead161d249010026bb98b825aeef985479a75
Chris Wood <chrispychris3@gmail.com>
2022-10-29 18:50:00 -0700

    Update home page styling and content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,22 +5,27 @@ import "../sass/home.scss";
 export default function Home(): ReactElement {
 
     return(
-      <div className="home__grid">
+    <div className="home__grid">
         <div className="home__initial-page-wrapper">
-          <h1 className="home__title">whoami</h1>
-          <p className="home__my-name">Chris Wood</p>
-          <h2 className="home__whatido">whatido</h2>
-          <p className="home__about">Full stack web developer. Systems administration. Webhosting/Server setup</p>
+            <h1 className="home__title">whoami</h1>
+            <p className="home__my-name">Chris Wood</p>
+            <h2 className="home__whatido">whatido</h2>
+            <p className="home__about">Full stack web developer.<br />Systems administration.<br />Webhosting/Server setup.</p>
         </div>
-
       
-        <div className="home__work-wrapper">
-          <p className="home__work">Want to hire me or would you like to collaborate on something? Shoot me an email and let's see if I can help!</p>
-        </div>
+        <div className="home__second-page-wrapper">
+            <div className="home__separator" />
 
-        <div className="home__projects-wrapper">
-          <p className="home__projects">Click <Link to="/projects" className="home__projects-link">here</Link> to check out some of the projects I've worked on, or check out my GitHub.</p>
+            <div className="home__work-wrapper">
+                <p className="home__work">Would you like to hire me or collaborate on something?<br /><br />Shoot me an email and let's see if I can help!</p>
+            </div>
+
+            <div className="home__separator home__separator--right" />
+
+            <div className="home__projects-wrapper">
+                <p className="home__projects">Click <Link to="/projects" className="home__projects-link">here</Link> to check out some of the projects I've worked on, or check out my GitHub.</p>
+            </div>
         </div>
-      </div>
+    </div>
     );
 }
