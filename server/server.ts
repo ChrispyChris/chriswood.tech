7399d11693268ef319f1c45ebf3372e6cc13a207
Chris Wood <chrispychris3@gmail.com>
2022-11-08 01:21:59 -0800

    Add new code

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import sqlite from "sqlite3";
 
+const server = express();
+
+// Load the database and check for errors.
 let db = new sqlite.Database("./blog_database.db", (error) => {
     if (error) {
         console.log("There was an error opening the database.");
@@ -9,4 +12,12 @@ let db = new sqlite.Database("./blog_database.db", (error) => {
     console.log("Connected to database.");
 });
 
+const port = 3001
 
+server.get('/', (req, res) => {
+  res.send(new Date().toLocaleTimeString())
+})
+
+server.listen(port, () => {
+  console.log(`Example app listening at 127.0.0.1:${port}`)
+})
