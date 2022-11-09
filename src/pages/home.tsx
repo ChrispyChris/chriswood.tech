123adc60b7beb785f4b0ce32183622674f52c74f
Chris Wood <chrispychris3@gmail.com>
2022-11-08 23:10:14 -0800

    Update home styling

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,13 +17,15 @@ export default function Home(): ReactElement {
             <div className="home__separator" />
 
             <div className="home__work-wrapper">
-                <p className="home__work">Would you like to hire me?<br /><br />Are you interested in collaborating on something?<br /><br />Shoot me an email and let's see if I can help!</p>
+                <p className="home__work">Would you like to hire me?</p>
+                <p className="home__work">Are you interested in collaborating on something?</p>
+                <p className="home__work">Shoot me an email and let's see if I can help!</p>
             </div>
 
             <div className="home__separator home__separator--right" />
 
             <div className="home__projects-wrapper">
-                <p className="home__projects">Click <Link to="/projects" className="home__projects-link">here</Link> to check out some of the projects I've worked on, or check out my GitHub.</p>
+                <p className="home__projects">Click <Link to="/projects" className="projects__link projects__link--first">here</Link> to check out some of the projects I've worked on, or check out my <a className="projects__link" href="https://github.com/ChrispyChris">GitHub.</a></p>
             </div>
         </div>
     </div>
