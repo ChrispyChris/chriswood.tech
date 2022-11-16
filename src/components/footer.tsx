bd26bc8de3987d09577d75a2402082b2998b0bac
Chris Wood <chrispychris3@gmail.com>
2022-11-15 16:04:07 -0800

    Finish home page styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,7 @@ import "../sass/footer.scss";
 export default function Footer(): ReactElement {
     return(
       <div className="footer">
-        <span className="footer__copyright">&#169;&nbsp;</span>
-        chriswood.tech
+        <span className="footer__copyright">&#169;&nbsp; chriswood.tech</span>
       </div>
     );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,22 +10,24 @@ export default function Home(): ReactElement {
             <h1 className="home__title">whoami</h1>
             <p className="home__my-name">Hi, I'm <span className="home__my-name--colored">Chris Wood</span>, a self-taught developer with a passion for programming, Linux systems and server administration.</p>
             <h2 className="home__whatido">whatido</h2>
-            <p className="home__about">Full stack web development.<br />System administration.<br />Webhosting/Server setup.</p>
+            <ul className="home__about">
+                <li className="about__item">Full stack web development.</li>
+                <li className="about__item">System administration.</li>
+                <li className="about__item">Webhosting/Server setup.</li>
+                <li className="about__item">Application and hosting management.</li>
+            </ul>
         </div>
       
         <div className="home__second-page-wrapper">
-            <div className="home__separator" />
-
+            <div className="home__separator-wrapper"><div className="home__separator" /></div>
             <div className="home__work-wrapper">
                 <p className="home__work">Would you like to hire me?</p>
                 <p className="home__work">Are you interested in collaborating on something?</p>
                 <p className="home__work">Shoot me an email and let's see if I can help!</p>
             </div>
-
-            <div className="home__separator home__separator--right" />
-
+            <div className="home__separator-wrapper home__separator-wrapper--right"><div className="home__separator" /></div>
             <div className="home__projects-wrapper">
-                <p className="home__projects">Click <Link to="/projects" className="projects__link projects__link--first">here</Link> to check out some of the projects I've worked on, or check out my <a className="projects__link" href="https://github.com/ChrispyChris">GitHub.</a></p>
+                <p className="home__projects">Click <Link to="/projects" className="projects__link projects__link--first">here</Link> to check out some of the projects I've worked on, or check out my <a className="projects__link" href="https://github.com/ChrispyChris">GitHub</a>.</p>
             </div>
         </div>
     </div>
